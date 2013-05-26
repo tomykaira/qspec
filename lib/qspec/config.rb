6accbedaa15f5db5da299419ff92ffcef2bf5fe8
tomykaira <tomykaira@gmail.com>
2013-05-26 18:40:20 +0900

    Add guidance about config file

--- lib/qspec/config.rb
+++ lib/qspec/config.rb
@@ -2,21 +2,28 @@ require 'yaml'
 
 module Qspec
   class Config
-    def initialize(path = '.qspec.yml')
+    DEFAULT_PATH = '.qspec.yml'
+
+    def initialize(path = DEFAULT_PATH)
       @path = path
-      @config = File.exists?(path) && YAML.load_file(path)
+      if File.exists?(path)
+        @config = YAML.load_file(path)
+      else
+        STDERR.puts "You do not have #{path}.  Initialize with `qspec-helper init`"
+        exit 1
+      end
     end
 
     def [](key)
       @config[key]
     end
 
-    def create_template
-      if File.exists?(@path)
-        STDERR.puts "You already have a template file #{@path}.  Remove it if you want to reset."
+    def self.create_template(path = DEFAULT_PATH)
+      if File.exists?(path)
+        STDERR.puts "You already have a template file #{path}.  Remove it if you want to reset."
         exit 1
       end
-      File.open(@path, 'w') do |f|
+      File.open(path, 'w') do |f|
         f.write <<TMPL
 # DO NOT check this file into VCS (e.g. git).
 # Parallelization setting differs between machines.
@@ -26,6 +33,7 @@ sort_by: time     # 'time' or 'size',  if 'time', store execution time and use i
 workers: 4  # half of cpu - number of cpu
 TMPL
       end
+      puts "Config file created in #{path}. Check it before run `qspec spec`."
     end
   end
 end
--- lib/qspec/helper.rb
+++ lib/qspec/helper.rb
@@ -4,15 +4,15 @@ module Qspec
 
     def initialize(argv)
       @argv   = argv
-      @config = Config.new
     end
 
     def serve
       case @argv.last
       when 'init'
         puts "Creating template"
-        Config.new.create_template
+        Config.create_template
       when 'spork'
+        @config = Config.new
         puts "Start #{@config['workers']} sporks"
         start_spork_workers(@config['workers'])
       end
